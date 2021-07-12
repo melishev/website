95c7da07a754f09886613b3badf3422e62479281
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-07-12 23:37:01 +0300

    🍀 Add Sass

--- components/header.js
+++ components/header.js
@@ -1,18 +1,14 @@
-import { Page, Text } from '@geist-ui/react'
+import styles from 'styles/modules/header.module.scss'
+
+import { Page, Text, Loading } from '@geist-ui/react'
 
 const header = () => {
-
-  const style = {
-     backdropFilter: `saturate(180%) blur(5px)`,
-     position: `sticky`,
-     top: 0,
-     zIndex: 999,
-     borderBottom: `1px solid red`
-  }
   
   return (
-    <Page.Header style={style}>
-      <Text h2>Header</Text>
+    <Page.Header className={styles.Header}>
+      <Text h1 size="2rem">Melishev</Text>
+      <Loading />
+      <Loading />
     </Page.Header>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,37 +5,35 @@ import Footer from 'components/footer'
 
 export default function Home() {
   return (
-    <>
+    <Page size="100%" style={{padding: 0}}>
       <Header />
-      <Page size="100%">
-        <Page.Content>
-          <Card width="330px">
-            <h4>Geist UI React</h4>
-            <p>Modern and minimalist React UI library.</p>
-            <Card.Footer>
-              <Link color target="_blank" href="https://github.com/geist-org/react">Visit source code on GitHub.</Link>
-            </Card.Footer>
-          </Card>
-          <Card width="400px">
-            <Image
-              src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
-              alt="lol"
-              height="200"
-              width="400"
-              style={{ objectFit: 'cover' }}
-            />
-            <Text h4 style={{ marginBottom: '0' }}>Geist UI React</Text>
-            <Text type="secondary" small>Modern and minimalist React UI library.</Text>
-            <Card.Footer>
-              <Link block target="_blank" href="https://github.com/geist-org/react">Visit source code on GitHub.</Link>
-            </Card.Footer>
-          </Card>
-          <Image.Browser url="https://react.geist-ui.dev/en-us/guide/introduction" invert>
-            <Image width={540} height={246} src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png" alt="lol" />
-          </Image.Browser>
-        </Page.Content>
-      </Page>
+      <Page.Content>
+        <Card width="330px">
+          <h4>Geist UI React</h4>
+          <p>Modern and minimalist React UI library.</p>
+          <Card.Footer>
+            <Link color target="_blank" href="https://github.com/geist-org/react">Visit source code on GitHub.</Link>
+          </Card.Footer>
+        </Card>
+        <Card width="400px">
+          <Image
+            src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png"
+            alt="lol"
+            height="200"
+            width="400"
+            style={{ objectFit: 'cover' }}
+          />
+          <Text h4 style={{ marginBottom: '0' }}>Geist UI React</Text>
+          <Text type="secondary" small>Modern and minimalist React UI library.</Text>
+          <Card.Footer>
+            <Link block target="_blank" href="https://github.com/geist-org/react">Visit source code on GitHub.</Link>
+          </Card.Footer>
+        </Card>
+        <Image.Browser url="https://react.geist-ui.dev/en-us/guide/introduction" invert>
+          <Image width={540} height={246} src="https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png" alt="lol" />
+        </Image.Browser>
+      </Page.Content>
       <Footer />
-    </>
+    </Page>
   )
 }

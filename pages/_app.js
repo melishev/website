4da2b2adbc95331c7bfa2d0d2c2846f9047cd29d
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-07-20 23:24:05 +0300

    🍀 Add Seo and Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,50 @@
 import 'styles/global.scss'
 
+import { Provider } from 'react-redux'
+import { store } from '../redux/store'
+
+import { DefaultSeo } from 'next-seo'
+
 import Header from 'components/header'
 import Back from 'components/back'
 import ModalNavigation from 'components/modalNavigation'
 import Footer from 'components/footer'
 
-import { Provider } from 'react-redux'
-import { store } from '../redux/store'
-
 function Application({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <DefaultSeo
+        title='Main'
+        titleTemplate='%s | Melishev'
+        description='Melishev ™ is a space where works, news and products created by creative and talented people are demonstrated.'
+        canonical='https://melishev.ru/'
+        openGraph={{
+          type: 'website',
+          url: 'https://melishev.ru',
+          site_name: 'Melishev ™',
+          title: 'Main | Melishev',
+          description: 'Melishev ™ is a space where works, news and products created by creative and talented people are demonstrated.',
+          images: [
+            {
+              url: 'https://melishev.ru/ogImage.jpg',
+              width: 1200,
+              height: 630,
+              alt: 'Community',
+            },
+          ],
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: 'https://melishev.ru/favicon.svg',
+          },
+          {
+            rel: 'mask-icon',
+            href: 'https://melishev.ru/favicon.svg',
+            color: 'black',
+          },
+        ]}
+      />
       <Header />
       <Component {...pageProps} />
       <Back />

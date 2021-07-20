88c1d7edad93a61c9189be9ef0f9aaa272f04010
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-07-20 19:36:48 +0300

    🍀 Changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import styles from 'styles/pages/home.module.scss'
 
+import { useState } from 'react'
+
 function Home() {
+
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Thanks`)
+
+    setEmail('')
+  }
+
   return (
     <main className={styles.Home}>
       <h1>In currently time, web application not available yet.</h1>
       <h2>But in August 2021 everything will change!</h2>
       <h3>Subscribe to updates.</h3>
-      <form>
-        <input type="email" placeholder="E-mail" required />
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} required />
         <button type="submit">Send</button>
       </form>
     </main>

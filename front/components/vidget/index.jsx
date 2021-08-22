6aa7f026b56b6bc26b92bb149c2a1b3b3dd9c5e0
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-08-22 12:39:32 +0300

    📟 Added to Vidget

--- front/components/vidget/index.jsx
+++ front/components/vidget/index.jsx
@@ -1,16 +1,31 @@
 import styles from './styles.module.scss'
 
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import { ReactSVG } from 'react-svg'
 
 import { Small, Text } from 'components/typography'
 
 const Vidget = () => {
+
+  const [track, setTrack] = useState({})
+
+  useEffect(() => {
+    try {
+      axios.get(`https://website-services.dwarf.dk/api/current-track`)
+      .then(res => setTrack(res.data))
+    } catch (err) {
+      console.log(err);
+    }
+  }, [])
+  
   return (
     <div className={styles.Vidget}>
       <ReactSVG className={styles.Vidget_icon} src="/svg/boomBox.svg"/>
       <div className={styles.Vidget_info}>
-        <Text>DOROGO (feat. blago white)</Text>
-        <Small>Егор Крид, blago white</Small>
+        <Text>{track.trackname}</Text>
+        <Small>{track.artist}</Small>
       </div>
     </div>
   );

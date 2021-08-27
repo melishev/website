fafd910718be84328044541a9930007bda0baed9
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-08-27 11:29:15 +0300

    🍀 New styles

--- front/components/index.js
+++ front/components/index.js
@@ -3,5 +3,9 @@ export { default as Footer } from './footer/footer'
 export { default as Modal } from './modal'
 export { default as Vidget } from './vidget'
 
+// Card
+export { default as CardNewsBig } from './cards/news/big'
+export { default as CardNewsSmall } from './cards/news/small'
+
 // Delete later
 export { default as Plug } from './plug/plug'

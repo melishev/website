9a84642dbf415578655c9dde042106a4904928ad
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-09-04 01:36:51 +0300

    🛠 Вывод статей по Slug

--- back/api/articles/controllers/articles.js
+++ back/api/articles/controllers/articles.js
@@ -5,4 +5,13 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils')
+
+module.exports = {
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.articles.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.articles })
+  }
+};

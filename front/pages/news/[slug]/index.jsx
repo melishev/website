7ffaf5d8c93641badacd25fb6bbb05dec4181c83
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-09-04 16:33:26 +0300

    🌵 Страница статьи

--- front/pages/news/[slug]/index.jsx
+++ front/pages/news/[slug]/index.jsx
@@ -1,19 +1,45 @@
 import styles from './styles.module.scss';
-import Layout from 'layouts/default';
+import propTypes from 'prop-types';
+
+import Layout from 'layouts/article';
 
 import { fetchArticleData } from 'utils/fetchPageData';
 
 import RenderText from 'utils/renderEditorJS';
 
-function Article({ article }) {
+import { DateTime } from 'luxon';
+import { MaskImage } from 'components/utils';
+
+function Article({ category, date, title, subTitle, media, article }) {
   return (
     <article className={styles.Article}>
+      <small className={styles.Article_date}>{`${category.name} | ${DateTime.fromISO(date).setLocale('ru').toFormat('DD')}`}</small>
+      <h1 className={styles.Article_title}>{title}</h1>
+      <h5 className={styles.Article_subtitle}>{subTitle}</h5>
+      <figure className={styles.Article_media}>
+        <MaskImage mask="2x1" src={media.url} alt={media.alternativeText} />
+        <figcaption><small>{media.alternativeText}</small></figcaption>
+      </figure>
       {JSON.parse(article).blocks.map((block) => (
         RenderText(block)
       ))}
     </article>
   );
 }
+
+Article.propTypes = {
+  category: propTypes.shape({
+    name: propTypes.string,
+  }).isRequired,
+  date: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  subTitle: propTypes.string.isRequired,
+  media: propTypes.shape({
+    alternativeText: propTypes.string,
+    url: propTypes.string,
+  }).isRequired,
+  article: propTypes.string.isRequired,
+};
 
 Article.getLayout = function getLayout(page) {
   return (

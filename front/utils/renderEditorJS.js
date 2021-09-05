f841a4295599c2df0fe30fa386dee437547057a6
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-09-05 14:52:16 +0300

    📝 Доработал модуль рендера текста

--- front/utils/renderEditorJS.js
+++ front/utils/renderEditorJS.js
@@ -1,4 +1,7 @@
-/* eslint-disable react/no-danger */
+/* eslint-disable react/no-danger, jsx-a11y/label-has-associated-control */
+
+import { useState } from 'react';
+import { MaskImage } from 'components/utils';
 
 // Данный модуль предназначен для преобразования данных из Editor.js в HTML
 // Модуль не должен как либо стилизовать компоненты! Его основная задача -
@@ -10,20 +13,177 @@
 function RenderHeading(block) {
   switch (block.data.level) {
     case 1:
-      return <h1 key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <h1 key={block.id} className="editor-h1" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
     case 2:
-      return <h2 key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <h2 key={block.id} className="editor-h2" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
     case 3:
-      return <h3 key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <h3 key={block.id} className="editor-h3" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
     case 4:
-      return <h4 key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <h4 key={block.id} className="editor-h4" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
     case 5:
-      return <h5 key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <h5 key={block.id} className="editor-h5" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
     case 6:
-      return <h6 key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <h6 key={block.id} className="editor-h6" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
     default:
-      return <p key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+      return <p key={block.id} className="editor-error">Error</p>;
   }
+}
+
+// Функция рендерит обычный текст
+function RenderParagraph(block) {
+  return <p key={block.id} className="editor-paragraph" dangerouslySetInnerHTML={{ __html: block.data.text }} />;
+}
+
+// Функция определяет стиль списка
+function RenderList(block) {
+  switch (block.data.style) {
+    case 'ordered':
+      return (
+        <ol key={block.id} className="editor-list">
+          {block.data.items.map((item) => (
+            <li key={block.id + item[0]}>
+              <p dangerouslySetInnerHTML={{ __html: item }} />
+            </li>
+          ))}
+        </ol>
+      );
+    case 'unordered':
+      return (
+        <ul key={block.id} className="editor-list" style={{ listStyle: 'disc' }}>
+          {block.data.items.map((item) => (
+            <li key={block.id + item[0]}>
+              <p dangerouslySetInnerHTML={{ __html: item }} />
+            </li>
+          ))}
+        </ul>
+      );
+    default:
+      return <p key={block.id} className="editor-error">Error</p>;
+  }
+}
+
+// Функция рендерит разделитеель текста
+function RenderDelimiter(block) {
+  return <hr key={block.id} className="editor-delimiter" />;
+}
+
+// Функиця рендера Open Graph
+function RenderLink(block) {
+  return (
+    <div key={block.id}>
+      <a href={block.data.link}>
+        <h6>{block.data.meta.title}</h6>
+        <p>{block.data.meta.description}</p>
+        <img src={block.data.meta.image.url} alt={block.data.meta.description} />
+      </a>
+    </div>
+  );
+}
+
+// Функция рендерит встройку кода
+function RenderCode(block) {
+  return (
+    <pre key={block.id} className="editor-code">
+      <code>{block.data.code}</code>
+    </pre>
+  );
+}
+
+// Функция рендерит цитату
+function RenderQuote(block) {
+  return (
+    <figure key={block.id} className="editor-quote" style={{ textAlign: block.data.alignment }}>
+      <blockquote>
+        <p dangerouslySetInnerHTML={{ __html: block.data.text }} />
+      </blockquote>
+      <city dangerouslySetInnerHTML={{ __html: block.data.caption }} />
+    </figure>
+  );
+}
+
+// Функция определяет есть ли у таблицы заголовок
+function RenderTable(block) {
+  const [data] = useState(block.data.content);
+  const [heading] = useState(data.shift());
+
+  switch (block.data.withHeadings) {
+    case true:
+      return (
+        <table key={block.id} className="editor-table">
+          <thead>
+            <tr>
+              {heading.map((item) => (
+                <td key={block.id + item} dangerouslySetInnerHTML={{ __html: item }} />
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row) => (
+              <tr key={block.id + row}>
+                {row.map((item) => (
+                  <td key={block.id + item} dangerouslySetInnerHTML={{ __html: item }} />
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    case false:
+      return (
+        <table key={block.id} className="editor-table">
+          <tbody>
+            {block.data.content.map((row) => (
+              <tr key={block.id + row}>
+                {row.map((item) => (
+                  <td key={block.id + item} dangerouslySetInnerHTML={{ __html: item }} />
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    default:
+      return <p key={block.id} className="editor-error">Error</p>;
+  }
+}
+
+// Функция рендера изображения
+function RenderImage(block) {
+  return (
+    <figure key={block.id} className="editor-image" data-border={block.data.withBorder} data-stretched={block.data.stretched} data-background={block.data.withBackground}>
+      <MaskImage mask="2x1" src={block.data.file.url} alt={block.data.file.alternativeText} />
+      <figcaption><small>{block.data.caption}</small></figcaption>
+    </figure>
+  );
+}
+
+// Функция рендерит список CheckBox
+function RenderChecklist(block) {
+  return (
+    <div key={block.id} className="editor-checklist">
+      {block.data.items.map((item) => (
+        <label key={block.id + item.text}>
+          <input type="checkbox" readOnly checked={item.checked} />
+          <p dangerouslySetInnerHTML={{ __html: item.text }} />
+        </label>
+      ))}
+    </div>
+  );
+}
+
+// Функция встраивает HTML
+function RenderRaw(block) {
+  return <div key={block.id} className="editor-raw" dangerouslySetInnerHTML={{ __html: block.data.html }} />;
+}
+
+// Функция рендерит сообщение
+function RenderWarning(block) {
+  return (
+    <div className="editor-warning" key={block.id}>
+      <h5 dangerouslySetInnerHTML={{ __html: block.data.title }} />
+      <p dangerouslySetInnerHTML={{ __html: block.data.message }} />
+    </div>
+  );
 }
 
 // Главная функция
@@ -32,16 +192,29 @@ function RenderText(block) {
     case 'header':
       return RenderHeading(block);
     case 'paragraph':
-      return (
-        <>
-          <p key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />
-          <br />
-        </>
-      );
+      return RenderParagraph(block);
+    case 'list':
+      return RenderList(block);
+    case 'delimiter':
+      return RenderDelimiter(block);
+    case 'link':
+      return RenderLink(block);
+    case 'code':
+      return RenderCode(block);
+    case 'quote':
+      return RenderQuote(block);
+    case 'table':
+      return RenderTable(block);
     case 'image':
-      return <p>IMAGE</p>;
+      return RenderImage(block);
+    case 'checklist':
+      return RenderChecklist(block);
+    case 'raw':
+      return RenderRaw(block);
+    case 'warning':
+      return RenderWarning(block);
     default:
-      return <p key={block.id}>Error</p>;
+      return <p key={block.id} className="editor-error">Error</p>;
   }
 }
 

93c131a5b9738e86f81f3d6cb60e3064cc85a10b
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-12-21 06:19:48 +0100

    feat: add module for simple and safe use env

---
+++ src/shared/config/env.ts
@@ -0,0 +1,10 @@
+const getEnvVar = (key: string) => {
+  if (import.meta.env[key] === undefined) {
+    throw new Error(`Env variable ${key} is required`);
+  }
+  return import.meta.env[key] || '';
+};
+
+export const isDevEnv = getEnvVar('DEV'); // Vite feature
+
+export const isProdEnv = getEnvVar('PROD'); // Vite feature

669d5a4484710154bfe8b0a19a361d0ad7e3446e
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-12-22 06:04:25 +0100

    refactor: replace assets file

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import { createApp } from 'vue';
 import App from './index.vue';
-import '@/assets/index.scss';
+import './assets/index.scss';
 
 const app = createApp(App);
 

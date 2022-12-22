522f859c5fd27ceeb745036e66b18bd9615be038
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-12-22 06:17:08 +0100

    feat: add global storage - Pinia

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,12 @@
 /* eslint-disable import/prefer-default-export */
+import { store } from '@/shared/config';
 import { createApp } from 'vue';
+
 import App from './index.vue';
 import './assets/index.scss';
 
 const app = createApp(App);
+
+app.use(store);
 
 export { app };
---
+++ src/shared/config/store.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import { createPinia } from 'pinia';
+
+const store = createPinia();
+
+export { store };

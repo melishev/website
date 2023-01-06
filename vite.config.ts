ec7700f4bfa43d809d2fd3a3d616de86a06e40d0
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2023-01-06 01:35:17 +0100

    feat: add pwa plugin for vite. replace static manifest to generate

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,37 @@ import path from 'path';
 
 import { defineConfig, type PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Melishev',
+        short_name: 'melishev - web dev',
+        description:
+          `Personal web application by Matvey Melishev,
+          for testing and demonstrating various cool technologies`,
+        icons: [
+          {
+            src: 'favicon/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#1B1B1B',
+        background_color: '#1B1B1B',
+        display: 'standalone',
+      },
+    }),
     visualizer() as PluginOption,
   ],
   resolve: {
